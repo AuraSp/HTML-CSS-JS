7ed9b4d6b6e1547e4d691ec87924542ad836df12
Aura <80409312+AuraSp@users.noreply.github.com>
2021-03-23 10:38:01 +0200

    Changed

---
+++ Grid/Grid.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $("#interactive-button").on("click", function () {
+        alert("Wow");
+    });
+});
+
+
+$(document).ready(function () {
+    $("button").click(function () {
+        $("div").animate({ left: '800px'});
+    });
+});
+
+$(document).ready(function () {
+    $("#interactive").on("click", function () {
+        alert("Wow");
+    });
+});
